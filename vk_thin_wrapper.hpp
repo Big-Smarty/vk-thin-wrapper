4625d8b7fee70b9d614e4468e3c69f60662afee0
Big Smarty <77147643+Big-Smarty@users.noreply.github.com>
2022-10-17 20:48:25 +0200

    made it actually work

--- vk_thin_wrapper.hpp
+++ vk_thin_wrapper.hpp
@@ -5,31 +5,36 @@
 #include <spdlog/spdlog.h>
 #include <stdexcept>
 #include <type_traits>
+
+#define VK_NO_PROTOTYPES
 #include <vulkan/vulkan_core.h>
 
 struct no_parent_t {};
 
+VkResult vkCreateSurfaceDummy(VkInstance, const no_parent_t *,
+                              const VkAllocationCallbacks *, VkSurfaceKHR *);
 namespace bs::thin_wrappers {
 template <typename T, typename CreateInfo, typename ParentType>
 using create_fn =
     std::conditional_t<std::is_same_v<ParentType, no_parent_t>,
                        VkResult (*)(const CreateInfo *,
                                     const VkAllocationCallbacks *, T *),
-                       VkResult (*)(const ParentType *, const CreateInfo *,
+                       VkResult (*)(ParentType, const CreateInfo *,
                                     const VkAllocationCallbacks *, T *)>;
 template <typename T, typename ParentType>
 using destroy_fn =
     std::conditional_t<std::is_same_v<T, VkInstance> ||
                            std::is_same_v<T, VkDevice>,
                        void (*)(T, const VkAllocationCallbacks *),
-                       void (*)(ParentType, const VkAllocationCallbacks *, T)>;
+                       void (*)(ParentType, T, const VkAllocationCallbacks *)>;
 template <typename T, typename CreateInfo, typename ParentType,
           create_fn<T, CreateInfo, ParentType> CreateFn,
           destroy_fn<T, ParentType> DestroyFn>
 class thin_wrapper {
 public:
-  thin_wrapper(const CreateInfo &create_info) requires
-      std::is_same_v<ParentType, no_parent_t> {
+  thin_wrapper(const CreateInfo &create_info)
+    requires std::is_same_v<ParentType, no_parent_t>
+  {
     try {
       auto result = CreateFn(&create_info, nullptr, &m_object);
       if (result != VK_SUCCESS) {
@@ -47,12 +52,11 @@ public:
       exit(-1);
     }
   }
-  thin_wrapper(const CreateInfo &create_info,
-               const ParentType
-                   &parent) requires(!std::is_same_v<ParentType, no_parent_t>)
+  thin_wrapper(const CreateInfo &create_info, const ParentType &parent)
+    requires(!std::is_same_v<ParentType, no_parent_t>)
       : m_parent(parent) {
     try {
-      auto result = CreateFn(&parent, &create_info, nullptr, &m_object);
+      auto result = CreateFn(parent, &create_info, nullptr, &m_object);
       if (result != VK_SUCCESS) {
         throw std::runtime_error(
             std::string("There was an error during VK object initialization! "
@@ -70,18 +74,22 @@ public:
   }
   thin_wrapper(const thin_wrapper &) = delete;
   thin_wrapper(thin_wrapper &&other) noexcept
-      requires(std::is_same_v<ParentType, no_parent_t>)
+    requires(std::is_same_v<ParentType, no_parent_t>)
       : m_object(std::exchange(other.m_object, nullptr)) {}
   thin_wrapper(thin_wrapper &&other) noexcept
-      requires(!std::is_same_v<ParentType, no_parent_t>)
+    requires(!std::is_same_v<ParentType, no_parent_t>)
       : m_object(std::exchange(other.m_object, nullptr)),
         m_parent(std::exchange(other.m_parent, nullptr)) {}
   thin_wrapper &operator=(const thin_wrapper &) = delete;
   thin_wrapper &operator=(thin_wrapper &&) = delete;
-  ~thin_wrapper() requires std::is_same_v<ParentType, no_parent_t> {
+  ~thin_wrapper()
+    requires std::is_same_v<ParentType, no_parent_t>
+  {
     DestroyFn(m_object, nullptr);
   }
-  ~thin_wrapper() requires(!std::is_same_v<ParentType, no_parent_t>) {
+  ~thin_wrapper()
+    requires(!std::is_same_v<ParentType, no_parent_t>)
+  {
     DestroyFn(m_parent, m_object, nullptr);
   }
 
@@ -90,6 +98,6 @@ public:
 
 protected:
   T m_object{nullptr};
-  [[no_unique_address]] ParentType m_parent;
+  [[no_unique_address]] ParentType m_parent{nullptr};
 };
 } // namespace bs::thin_wrappers
